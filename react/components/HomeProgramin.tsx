264cba2a64123edc237265b925873479b3aa9da4
CarlosGiovannyG <cggualtero@hotmail.com>
2022-08-17 14:25:17 -0500

    Adelanto en los botones de acción

--- react/components/HomeProgramin.tsx
+++ react/components/HomeProgramin.tsx
@@ -1,54 +1,70 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
-import { Table, PageBlock } from 'vtex.styleguide'
-// import {  PageBlock } from 'vtex.styleguide'
+import { Table, PageBlock, ButtonWithIcon, IconCalendar } from 'vtex.styleguide'
 import { data } from '../data/harcodedData'
-// import { schemaHome } from '../schemas/schemaHome'
+import { PropsData } from '../interfaces/interfaceData'
 
-
-const schemaHOme =
-{
+const SchemaHome = {
   properties: {
     idExistent: {
-      title:  <FormattedMessage
-      id="admin-programversion.header.table-id"
+      title: <FormattedMessage
+        id="admin-programversion.header.table-id"
       />,
       width: 300,
     },
     name: {
       // title: 'Name Version',
       title: <FormattedMessage
-      id="admin-programversion.header.table-name-version"
+        id="admin-programversion.header.table-name-version"
       />,
       width: 350,
     },
     date: {
       // title: 'Last Publication',
       title: <FormattedMessage
-      id="admin-programversion.header.table-last-publication"
+        id="admin-programversion.header.table-last-publication"
       />,
       width: 350,
     },
     state: {
       // title: 'State of Version',
       title: <FormattedMessage
-      id="admin-programversion.header.table-state-version"
+        id="admin-programversion.header.table-state-version"
       />,
       width: 350,
     },
     newDAte: {
       // title: 'New Publication',
       title: <FormattedMessage
-      id="admin-programversion.header.table-new-publication"
+        id="admin-programversion.header.table-new-publication"
       />,
       width: 350,
     },
     action: {
-      title: 'action'
-    }
-  },
-}
+      title: 'action',
+      cellRenderer: (rowData: PropsData) => {
+        return (
+          <div className='flex'>
+            <ButtonWithIcon
+              onClick={() => alert(JSON.stringify(rowData, null, 5))}
+              icon={<IconCalendar />}
+              variation='secondary'
+            />
+            <ButtonWithIcon
+              icon={<IconCalendar />}
+              variation='secondary'
+            />
+            <ButtonWithIcon
+              icon={<IconCalendar />}
+              variation='secondary'
+            />
 
+          </div >
+        )
+      }
+    }
+  }
+}
 
 export const HomeProgramin = () => {
   { console.log('===> INFORMATION LOG', data) }
@@ -56,12 +72,12 @@ export const HomeProgramin = () => {
     <div>
       <PageBlock variation="full">
         <Table
-          schema={schemaHOme}
+          schema={SchemaHome}
           items={data}
           toolbar={
             {
-              inputSearch:{
-                placeholder:'Busqueda'
+              inputSearch: {
+                placeholder: 'Busqueda'
               },
               density: {
                 buttonLabel: 'Line density',
@@ -76,4 +92,3 @@ export const HomeProgramin = () => {
     </div>
   )
 }
-
